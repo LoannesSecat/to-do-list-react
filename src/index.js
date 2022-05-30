87c539cb964085497c82e2a5b8b486e091c99a3b
Sebastian C <44909579+LoannesSecat@users.noreply.github.com>
2022-05-30 11:50:44 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-console.log(location)
-
 ReactDOM.render(
   <App />
   , document.getElementById('root')
